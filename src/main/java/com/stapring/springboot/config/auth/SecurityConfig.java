8aaba13f188b49930eebc6f8fd4e84cd2a811a02
DJY <djy1212@gmail.com>
2021-01-28 11:32:59 +0900

    modify security settring

--- src/main/java/com/stapring/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/stapring/springboot/config/auth/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .headers().frameOptions().disable()
                 .and()
                     .authorizeRequests()
-                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile" , "/login").permitAll()
+                    .antMatchers("/", "/css/**", "/vendor/**", "/images/**", "/js/**", "/h2-console/**", "/profile" , "/login").permitAll()
                     .antMatchers("/api/v1/**").hasRole(Role.USER.name())
                     .anyRequest().authenticated()
                 .and()
